2ffc063cfbad1950e01579ed18ef1dbea5042366
Estefi Turin <estefi.turin@gmail.com>
2024-10-10 12:39:42 -0300

    add spring boot project

---
+++ pet-backend/src/main/java/com/dev/petbackend/PetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.petbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBackendApplication.class, args);
+    }
+
+}
---
+++ pet-backend/src/test/java/com/dev/petbackend/PetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.petbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
