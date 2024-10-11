1505e436c919c5dba782a584cf29d074c3c68949
Estefi Turin <estefi.turin@gmail.com>
2024-10-11 12:37:58 -0300

    DTO's para manejo de registro de usuarios

--- pet-backend/src/main/java/com/dev/petbackend/controller/UserController.java
+++ pet-backend/src/main/java/com/dev/petbackend/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.dev.petbackend.controller;
 
+import com.dev.petbackend.exceptions.UserAlreadyExistsException;
 import com.dev.petbackend.model.User;
+import com.dev.petbackend.model.dto.EntityConverter;
+import com.dev.petbackend.model.dto.UserDto;
 import com.dev.petbackend.model.dto.request.RegistrationRequest;
+import com.dev.petbackend.model.dto.response.ApiResponse;
 import com.dev.petbackend.services.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final EntityConverter<User, UserDto> entityConverter;
 
     @PostMapping
-    public User add(@RequestBody RegistrationRequest request) {
-        return userService.add(request);
+    public ResponseEntity<ApiResponse> add(@RequestBody RegistrationRequest request) {
+        try {
+            User theUser = userService.add(request);
+            UserDto registeredUser = entityConverter.mapEntityToDto(theUser, UserDto.class);
+            return ResponseEntity.ok(new ApiResponse("User registered successfully", registeredUser));
+
+        } catch (UserAlreadyExistsException e) {
+            return ResponseEntity.ok(new ApiResponse(e.getMessage(), null));
+        }
     }
 }
---
+++ pet-backend/src/main/java/com/dev/petbackend/model/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.dev.petbackend.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String userType;
+    private boolean isEnable;
+    private String specialization;
+}
+
